ce265343fd8cd5e9ff1ff554aae45485b0b7d198
Justine Berretta <justberretta@gmail.com>
2019-05-23 03:29:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 2 
     "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
     "#{array[0..-2].join(", ")}, and #{array.last}" 
   end 
